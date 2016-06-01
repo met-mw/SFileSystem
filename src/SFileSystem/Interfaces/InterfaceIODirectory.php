ed881e2752cd5cb14f091eed777b039473059e29
met-mw <met-mw@rambler.ru>
2016-06-01 18:33:54 +0300

    PHP-Doc

--- src/SFileSystem/Interfaces/InterfaceIODirectory.php
+++ src/SFileSystem/Interfaces/InterfaceIODirectory.php
@@ -7,10 +7,29 @@ interface InterfaceIODirectory extends InterfaceIO
 
     public function createDirectory($directoryName);
     public function createFile($fileName);
+
+    /**
+     * @param string $directoryName
+     * @return InterfaceIODirectory
+     */
     public function getDirectory($directoryName);
+
+    /**
+     * @return InterfaceIODirectory[]
+     */
     public function getDirectories();
+
+    /**
+     * @param string $fileName
+     * @return InterfaceIOFile
+     */
     public function getFile($fileName);
+
+    /**
+     * @return InterfaceIOFile[]
+     */
     public function getFiles();
+
     public function scan($recursive = false);
 
 }
